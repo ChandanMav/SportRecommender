cf7b785f7592b4371ad140e0e0988963dced963e
chandan <chandan02kumar09@gmail.com>
2022-01-11 17:06:48 +0530

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgwWowModule } from 'ngx-wow';
+import { OwlModule } from 'ngx-owl-carousel'; 
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { BannerComponent } from './banner/banner.component';
+import { FormInputComponent } from './form-input/form-input.component';
+import { HeaderComponent } from './header/header.component';
+import { MessageComponent } from './message/message.component';
+import { ServiceComponent } from './service/service.component';
+import { FooterComponent } from './footer/footer.component';
+import { TeamComponent } from './team/team.component';
+import { ReportComponent } from './report/report.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    BannerComponent,
+    FormInputComponent,
+    HeaderComponent,
+    MessageComponent,
+    ServiceComponent,
+    FooterComponent,
+    TeamComponent,
+    ReportComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    NgwWowModule,
+    OwlModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/banner/banner.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-banner',
+  templateUrl: './banner.component.html',
+  styleUrls: ['./banner.component.css']
+})
+export class BannerComponent implements OnInit {
+
+  customOptions: any = {
+    loop: true,
+    margin: 10,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: true
+      },
+      600: {
+        items: 3,
+        nav: false
+      },
+      1000: {
+        items: 3,
+        nav: true,
+        loop: false
+      }
+    }
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
